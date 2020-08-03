046883221db1099d8339546a7bb7cec32b25b8d0
Jeovanni <jeovannildg@gmail.com>
2020-08-02 21:53:29 -0600

    added styles

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './Character.css';
 import {
-    Card, Button, CardImg, CardTitle, CardText, CardColumns,
-    CardSubtitle, CardBody
+    Card, CardImg, CardTitle, CardText, CardSubtitle, CardBody
   } from 'reactstrap';
+  
 
 const Character = (character) => {
     console.log(character.characters);
@@ -10,18 +11,18 @@ const Character = (character) => {
         // <div className = "character-container">
         //     <h2>{character.props.name}</h2>
         // </div>
-        <CardColumns>
-      <Card>
-        <CardImg top width="20%" src={character.props.image} alt="Card image cap" />
-        <CardBody>
-          <CardTitle>Name: {character.props.name}</CardTitle>
-          <CardSubtitle>Gender: {character.props.gender}</CardSubtitle>
-          <CardText>Created: {character.props.created}</CardText>
-          <CardText>Located in {character.props.location.name}, this character is {character.props.status}.</CardText>
-
-        </CardBody>
-      </Card>
-    </CardColumns>
+        <div className="card-container">
+            <Card body outline color="primary" >
+                <CardImg top width="20%" src={character.props.image} alt="Card image cap" />
+                <CardBody>
+                    <CardTitle>Name: {character.props.name}</CardTitle>
+                    <CardSubtitle>Gender: {character.props.gender}</CardSubtitle>
+                    <CardText>Created: {character.props.created}</CardText>
+                    <CardText>Located in {character.props.location.name}, this character is {character.props.status}.</CardText>
+                </CardBody>
+            </Card>
+        </div>
+      
     )
 
 }

a63ea1603aef5056449e36dd0f94fb6f722b300e
Jeovanni <jeovannildg@gmail.com>
2020-08-02 20:38:36 -0600

    started character component

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,12 @@
-// Write your Character component here
+import React from 'react';
+
+const Character = (data) => {
+    return (
+        <div className = "charaacter-container">
+            <h1>Character Name</h1>
+        </div>
+    )
+
+}
+
+export default Character;

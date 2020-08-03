0fe547ee7e6cb6f8cda875b2f637e55307c7ee4a
Jeovanni <jeovannildg@gmail.com>
2020-08-02 21:34:59 -0600

    completed cards

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -14,41 +14,12 @@ const Character = (character) => {
       <Card>
         <CardImg top width="20%" src={character.props.image} alt="Card image cap" />
         <CardBody>
-          <CardTitle>{character.props.name}</CardTitle>
-          <CardSubtitle>{character.props.gender}</CardSubtitle>
-          <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
-          <Button>Button</Button>
+          <CardTitle>Name: {character.props.name}</CardTitle>
+          <CardSubtitle>Gender: {character.props.gender}</CardSubtitle>
+          <CardText>Created: {character.props.created}</CardText>
+          <CardText>Located in {character.props.location.name}, this character is {character.props.status}.</CardText>
+
         </CardBody>
-      </Card>
-      <Card>
-        <CardImg top width="100%" src="/assets/256x186.svg" alt="Card image cap" />
-      </Card>
-      <Card>
-        <CardBody>
-          <CardTitle>Card title</CardTitle>
-          <CardSubtitle>Card subtitle</CardSubtitle>
-          <CardText>This card has supporting text below as a natural lead-in to additional content.</CardText>
-          <Button>Button</Button>
-        </CardBody>
-      </Card>
-      <Card body inverse style={{ backgroundColor: '#333', borderColor: '#333' }}>
-        <CardTitle>Special Title Treatment</CardTitle>
-        <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
-        <Button>Button</Button>
-      </Card>
-      <Card>
-        <CardImg top width="100%" src="/assets/256x186.svg" alt="Card image cap" />
-        <CardBody>
-          <CardTitle>Card title</CardTitle>
-          <CardSubtitle>Card subtitle</CardSubtitle>
-          <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</CardText>
-          <Button>Button</Button>
-        </CardBody>
-      </Card>
-      <Card body inverse color="primary">
-        <CardTitle>Special Title Treatment</CardTitle>
-        <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
-        <Button color="secondary">Button</Button>
       </Card>
     </CardColumns>
     )
